358c96cb0037a800076ae89d21a87bd036d543f9
vivekjoshi2006 <vivekjoshi41996@gmail.com>
2026-02-05 10:12:57 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,15 @@
 variable "key_name" {}
-resource "aws_instance" "strapi_server" { ami = "ami-0c55b159cbfafe1f0"; instance_type = "t2.micro"; key_name = var.key_name }
-output "public_ip" { value = aws_instance.strapi_server.public_ip }
+
+resource "aws_instance" "strapi_server" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Strapi-Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.strapi_server.public_ip
+}
