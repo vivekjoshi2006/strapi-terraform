3b55bc3b034a8702bfb9f29d49ffbca3f042a2ac
vivekjoshi2006 <vivekjoshi41996@gmail.com>
2026-02-05 10:12:38 +0530

    Update main.tf

--- modules/ssh-key/main.tf
+++ modules/ssh-key/main.tf
@@ -1,4 +1,18 @@
-resource "tls_private_key" "strapi_key" { algorithm = "RSA"; rsa_bits = 4096 }
-resource "aws_key_pair" "deployer" { key_name = "strapi-key"; public_key = tls_private_key.strapi_key.public_key_openssh }
-resource "local_file" "private_key" { content = tls_private_key.strapi_key.private_key_pem; filename = "${path.root}/strapi-key.pem" }
-output "key_name" { value = aws_key_pair.deployer.key_name }
+resource "tls_private_key" "strapi_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "strapi-key"
+  public_key = tls_private_key.strapi_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.strapi_key.private_key_pem
+  filename = "${path.root}/strapi-key.pem"
+}
+
+output "key_name" {
+  value = aws_key_pair.deployer.key_name
+}
